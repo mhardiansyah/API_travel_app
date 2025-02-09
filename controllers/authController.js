dea5a3c7036af5f8fa5d243f3924e640d100d1cb
mhardiansyah <mhardiansyah5766@gmail.com>
2025-02-09 09:45:47 +0700

    second commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,10 +5,15 @@ const db = require("../config/db");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
